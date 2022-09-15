29a288ba5909806b3ab1e0c49381a2f9d0cf03aa
caroline cloughley <caro672g@stud.kea.dk>
2022-09-15 11:11:53 +0200

    updated

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
+"usestrict";
+
+document.addEventListener("DOMContentLoaded", count);
+
 let counter = 0;
 let result;
 result = Array.from(counter);
 
-count();
-
 function count() {
   result.unshift(counter);
-  setTimeout(() => {
-    count();
-  }, 500);
+  setTimeout(count, 500);
   console.log(result.slice(0, 9));
   counter++;
 }
-
-//push= add something at the end
-//add counter plus one to the end
-//then add that new number plus one to the end
-//then add that new number plus one to the end
-//then add that new number plus one to the end
